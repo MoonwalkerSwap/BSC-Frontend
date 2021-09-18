0f105ce5df116c924e3359e3ba29b0f88db32ce5
MoonWalker <7510709+MoonWalkerJim@users.noreply.github.com>
2021-09-18 21:49:22 +0100

    Latests

--- src/views/Ifos/components/IfoTabButtons/index.tsx
+++ src/views/Ifos/components/IfoTabButtons/index.tsx
@@ -17,10 +17,10 @@ const IfoTabButtons = () => {
     <Wrapper>
       <ButtonMenu activeIndex={!isExact ? 1 : 0} scale="sm" variant="subtle">
         <ButtonMenuItem as={Link} to={`${url}`}>
-          Next IDO
+          Next IFO
         </ButtonMenuItem>
         <ButtonMenuItem as={Link} to={`${url}/history`}>
-          Past IDOs
+          Past IFOs
         </ButtonMenuItem>
       </ButtonMenu>
     </Wrapper>
