c742344ba56e9f04f1b313cbd949a842ad50eeb5
MoonWalker <7510709+MoonWalkerJim@users.noreply.github.com>
2021-09-18 21:44:26 +0100

    Latest

--- src/views/Ifos/components/Hero.tsx
+++ src/views/Ifos/components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
   return (
     <StyledHero>
       <Container>
-        <Title>{TranslateString(500, 'IDO: Initial DEX Offerings')}</Title>
+        <Title>{TranslateString(500, 'IFO: Initial Farm Offering')}</Title>
         <Blurb>{TranslateString(502, 'Buy new tokens with a brand new token sale model.')}</Blurb>
       </Container>
     </StyledHero>
