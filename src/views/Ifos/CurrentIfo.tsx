2ed255e13a11fad798f7ff52dbc0628b08ba58f7
MoonwalkerSwap <7510709+MoonWalkerJim@users.noreply.github.com>
2021-10-28 18:12:27 +0100

    updated

--- src/views/Ifos/CurrentIfo.tsx
+++ src/views/Ifos/CurrentIfo.tsx
@@ -55,7 +55,7 @@ const Ifo = () => {
             <LinkExternal href="https://bsc-swap.moonwalker.network/#/swap" mr="16px">
               {TranslateString(1060, 'Buy DUST')}
             </LinkExternal>
-            <LinkExternal href="https://bsc-swap.moonwalker.network/add/BNB/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82">
+            <LinkExternal href="https://bsc-swap.moonwalker.network/#/add/BNB/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82">
               {TranslateString(1062, 'Get MoonWalker LP tokens')}
             </LinkExternal>
           </Flex>
